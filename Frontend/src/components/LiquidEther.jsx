4766562009d879c34a68aabc23e24fccc16e79c8
giselarivas2112 <00016124@uca.edu.sv>
2025-11-13 07:11:36 -0600

    fix: Arreglar la ruta de estilos

--- Frontend/src/components/LiquidEther.jsx
+++ Frontend/src/components/LiquidEther.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react';
 import * as THREE from 'three';
-import "../styles/auth.css";
+import "../styles/style_cuenta.css";
 
 export default function LiquidEther({
   mouseForce = 20,
