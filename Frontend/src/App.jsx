0f84a3e3237e136be2f0129385c41e9aeebc0b50
giselarivas2112 <00016124@uca.edu.sv>
2025-11-13 05:55:27 -0600

    feat: Agregar nuevas rutas de inicio de login y registro

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,8 @@ import Home from "./pages/Home";
 import Descarga from "./pages/Descarga";
 import DetalleJuego from "./pages/DetalleJuego";
 import CategoriaPage from "./pages/CategoriaPage";
+import Login from "./pages/Login";
+import Register from "./pages/Registro";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Route path="/descarga" element={<Descarga />} /> {/* Página de descarga */}
         <Route path="/juego/:id" element={<DetalleJuego />} /> {/* Página de detalle */}
         <Route path="/categoria/:nombre" element={<CategoriaPage />} /> {/* pagina de categoria */}
+        <Route path="/login" element={<Login />} />   {/* Página de inicio de sesión */}
+        <Route path="/register" element={<Register />} /> {/* Página de registro */}  
       </Routes>
     </Router>
   );
