abbee30f94abb9a1c772834486235a0b0f7d6595
giselarivas2112 <00016124@uca.edu.sv>
2025-11-24 05:09:54 -0600

    fix: arreglar rutas de juego

--- Backend/routes/juegosRoutes.js
+++ Backend/routes/juegosRoutes.js
@@ -1,22 +1,29 @@
-// routes/favoritoRoutes.js
 import express from "express";
-
-import { getRelease, getOfertas, getTop, getAllGames, getGameById, getGamesByPlatform, getGamesByCategory, getGamesBySort} from "../controllers/Juegos/gameController.js";
-
+import {
+  getRelease,
+  getOfertas,
+  getTop,
+  getAllGames,
+  getGameById,
+  getGamesByPlatform,
+  getGamesByCategory,
+  getGamesBySort,
+} from "../controllers/Juegos/gameController.js";
 
 const router = express.Router();
 
 router.get("/release", getRelease);
 router.get("/ofertas", getOfertas);
 router.get("/top", getTop);
-router.get("/", getAllGames);
-router.get("/:id", getGameById);
+
 router.get("/platform/:platform", getGamesByPlatform);
 router.get("/category/:category", getGamesByCategory);
 router.get("/sort/:orden", getGamesBySort);
-// router.get("/search", getGamesBySearch);
-// router.get("/search", searchGames);
 
 
+router.get("/", getAllGames);
+
+
+router.get("/:id", getGameById);
 
 export default router;
