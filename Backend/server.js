39fcba6fc534e1ee09041d157a592f532584d14a
giselarivas2112 <00016124@uca.edu.sv>
2025-11-25 07:23:31 -0600

    refactor: Organizar y limpiar código del archivo server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,6 @@
 import express from "express";
 import cors from "cors";
 
-// import { PORT } from './keys/keys.js';
-
 import userRoutes from "./routes/userRoutes.js";
 import favoritoRoutes from "./routes/favoritoRoutes.js";
 import juegosRoutes from "./routes/juegosRoutes.js";
@@ -14,47 +12,12 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use(cors());
 
-// app.use(cors({
-//   origin: "*", 
-//   credentials: true
-// }));
 
 app.use("/api/users", userRoutes);
 
 app.use("/api/favoritos", favoritoRoutes);
 
 app.use("/api/games", juegosRoutes);
-
-// app.get("/api/games/search", async (req, res) => {
-//   const q = (req.query.q || "").trim();
-//   if (!q) return res.json([]);
-
-//   const norm = (s) =>
-//     (s || "")
-//       .toString()
-//       .normalize("NFD")
-//       .replace(/[\u0300-\u036f]/g, "")
-//       .toLowerCase();
-
-//   try {
-//     const respuesta = await fetch("https://www.freetogame.com/api/games");
-//     const datos = await respuesta.json();
-
-//     const nq = norm(q);
-//     const results = datos
-//       .filter((g) => {
-//         const t = norm(g.title);
-//         const d = norm(g.short_description);
-//         return t.includes(nq) || d.includes(nq);
-//       })
-//       .slice(0, 10); 
-
-//     res.json(results);
-//   } catch (error) {
-//     console.error("Error endpoint:", error);
-//     res.status(500).json({ error: "Error al buscar juegos" });
-//   }
-// });
 
 
 app.listen(PORT, () => {
